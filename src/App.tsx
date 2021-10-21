708a16220f6a4493c2577809ebf24d01c1e4e4af
Dblind <xxxx@zzz.yy>
2021-10-21 11:14:55 +0300

    first step

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,66 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { BlockLike } from 'typescript';
+
+const cl = console.log;
+
+
+let a: number | null | string | undefined = 10;
+a = undefined;
+
+let gender: "male" | "female";
+gender = "male";
+
+let names: Array<string> = ["Ann"];
+let names2: string[] = ["Bob"];
+
+
+type UserType = {
+  sayHello: Function,
+  sayBye?: (message: string) => void,
+  name: string,
+  age: number,
+  gender?: string,
+}
+
+let user: UserType = {
+  sayHello: function(f: Boolean) { return {}},
+  name: "Ann",
+  age: 12,
+  // condition: "have",
+}
+
+const summ: (a: number, b: number) => number = (a, b) => {
+  return a + b;
+}
+cl(summ(2, 5));
+
+const initialState = {
+  some: null as string | null,
+  num: 20,
+  string: "string",
+}
+export type InitialStateType = typeof initialState;
+
+
+const GET_TASKS = "APP/GetTasks";
+type GetTasksActionType = {
+  type: typeof GET_TASKS,   // <--- typeof
+  id: number,
+}
+
+let action: GetTasksActionType = {
+  type: GET_TASKS,
+  id: 28,
+}
+
+
+
+
+
+
+
 
 function App() {
   return (
